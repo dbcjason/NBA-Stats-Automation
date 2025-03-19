0f9abe6f071423172fbd33d182ef1b58f176a9af
dbcjason <dbbjasonb@gmail.com>
2025-03-19 01:16:39 -0500

    Update peak.py

--- peak.py
+++ peak.py
@@ -362,7 +362,7 @@ def make_predictions(player_group):
 
     sheet_name, model_filename = sheet_map[player_group]
 
-    model_filename = f"{group.lower()}_model.pkl"  # ✅ Save in the current directory
+    model_filename = f"{player_group.lower()}_model.pkl"  # ✅ Save in the current directory
     model = joblib.load(model_filename)
 
     
